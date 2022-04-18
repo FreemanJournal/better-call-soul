be4dd14b821d6bb49163c6142265617e01095a28
Md Ishaque <iamishaque.business@gmail.com>
2022-04-18 15:01:56 +0600

    update private route

--- src/routing/PrivateRoute.jsx
+++ src/routing/PrivateRoute.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 import auth from '../utilities/firebase.init';
 
 export default function PrivateRoute({ children }) {
-  const [user] = useAuthState(auth);
+  const [user,loading] = useAuthState(auth);
   const location = useLocation();
+  const customId = "custom-id-yes";
+  if(loading){
+    toast.info('Loading...', {
+      toastId: customId
+    })
+    return <ToastContainer position="top-center"/>
+  }
+
   if (!user) {
    return <Navigate to='/signIn' state={{ from: location }} replace />
   }
